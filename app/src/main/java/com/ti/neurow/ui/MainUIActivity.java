fd19871865ea906a1e674f903c96b5a6fbb3d5f1
dgumucio98 <diego.gumucio@tamu.edu>
2023-03-07 18:47:27 -0600

    - Added new launch animation - Added button (nonfunctional) for DB viewer in main screen - Changed app name and icon - Integrated main UI activity and splash activity into one - Deleted splash activity - Removed back buttons on login/register screens (keeping device back button)

--- app/src/main/java/com/ti/neurow/ui/MainUIActivity.java
+++ app/src/main/java/com/ti/neurow/ui/MainUIActivity.java
@@ -1,45 +1,92 @@
 package com.ti.neurow.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ti.neurow.R;
 import com.ti.neurow.ble.MainActivity;
 
-// Main Activity class (MainActivity is a subclass of AppCompactActivity, hence "extends")
 public class MainUIActivity extends AppCompatActivity {
 
-    // Declare class objects we will give functionality to
-    Button existingUser, newUser, BLEData; // buttons
-    ImageView rowerIcon; // only used for long-press toast message
+    // Declare views
+    ImageView rower, rowerIcon; // image
+    TextView neurowText, welcomeText; // text views
+    Button existingUser, newUser, BLEData, DBdata; // buttons
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) { // cold-start method to start activity
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         // Hide Action bar and Status bar
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
-        // Lock orientation to portrait (for this activity)
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Lock orientation to portrait
 
-        setContentView(R.layout.activity_ui_main); // specifies .xml layout to be used
+        setContentView(R.layout.activity_ui_main);
 
-        // Define elements (by ID in .xml)
+        // Animate rower icon and "Neurow" text
+        rower = (ImageView)findViewById(R.id.rower_icon);
+        neurowText = (TextView)findViewById(R.id.neurow_text);
+        welcomeText = (TextView)findViewById(R.id.txtWelcome);
         existingUser = findViewById(R.id.btnExistingUser);
         newUser = findViewById(R.id.btnNewUser);
         rowerIcon = findViewById(R.id.rower_icon);
         BLEData = findViewById(R.id.btnBLEData);
+        DBdata = findViewById(R.id.btnDBViewer);
+
+        Animation animation1 = AnimationUtils.loadAnimation(MainUIActivity.this, R.anim.slide_in_left);
+        Animation animation2 = AnimationUtils.loadAnimation(MainUIActivity.this, R.anim.slide_in_right);
+        rower.startAnimation(animation1);
+        neurowText.startAnimation(animation2);
+
+        // Handler: Move rower icon and make TextView disappear after 2.5 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rower.animate().translationYBy(-350).setDuration(1000).start();
+                neurowText.animate().alpha(0.0f).setDuration(700).start();
+            }
+        }, 2500);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                welcomeText.setVisibility(View.VISIBLE);
+                welcomeText.setAlpha(0f);
+                welcomeText.animate().alpha(1f).setDuration(700).start();
+
+                existingUser.setVisibility(View.VISIBLE);
+                existingUser.setAlpha(0f);
+                existingUser.animate().alpha(1f).setDuration(700).start();
+
+                newUser.setVisibility(View.VISIBLE);
+                newUser.setAlpha(0f);
+                newUser.animate().alpha(1f).setDuration(700).start();
+
+                BLEData.setVisibility(View.VISIBLE);
+                BLEData.setAlpha(0f);
+                BLEData.animate().alpha(1f).setDuration(700).start();
+
+                DBdata.setVisibility(View.VISIBLE);
+                DBdata.setAlpha(0f);
+                DBdata.animate().alpha(1f).setDuration(700).start();
+
+            }
+        }, 3500);
+
 
         // Existing user button listener
         existingUser.setOnClickListener(new View.OnClickListener() {
@@ -56,13 +103,13 @@ public class MainUIActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // Create intent to launch next activity (SignupActivity)
-                Intent i = new Intent(MainUIActivity.this, SignupActivity.class);
+                Intent i = new Intent(MainUIActivity.this, RegisterActivity.class);
                 startActivity(i); // Launch Signup
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
 
-        // TEMP: BLE Data View (Dev) button listener
+        // TEMP: BLE Data Viewer (Dev) button listener
         BLEData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,7 +119,17 @@ public class MainUIActivity extends AppCompatActivity {
             }
         });
 
-        // Easter egg: If rower icon is held
+        // TEMP: DB Data Viewer (Dev) button listener
+        BLEData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create intent to launch next activity (RawDataActivity)
+                Intent i = new Intent(MainUIActivity.this, MainActivity.class);
+                startActivity(i); // Launch BLE Data View
+            }
+        });
+
+        // Easter egg message: If rower icon is held
         rowerIcon.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
